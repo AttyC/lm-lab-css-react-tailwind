80ac53bb5e2c86a07bd94e4922183110c128671a
Atty Cronin <antoniajcronin@gmail.com>
2023-07-19 11:51:05 +0100

    Style Headers in App

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ function App() {
   return (
     <section>
       <h1>Welcome to Marvelous Mansions</h1>
-      <h2>A one stop shop to your dream property!</h2>
-      <h3>Current property listings:</h3>
+      <h2 className='font-medium uppercase p-4'>
+        A one stop shop to your dream property!
+      </h2>
+      <h3 className='text-left font-bold'>Current property listings:</h3>
       <PropertiesDisplay />
     </section>
   );
